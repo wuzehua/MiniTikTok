e4d13d269975ad6e5489a701dd0daf141d8159bc
RainZhong <1322030678@qq.com>
2019-07-14 12:49:26 +0800

    add Adapter

---
+++ app/src/main/java/com/bytedance/minitiktok/recyclerview/VideoListViewAdapter.kt
@@ -0,0 +1,20 @@
+package com.bytedance.minitiktok.recyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
