812e5f7199dab29c06c1113705f79a376249b1c7
shenmishajing <shenmishajing@Gmail.com>
2019-07-17 16:20:27 +0800

    format code

--- app/src/main/java/com/bytedance/minitiktok/recyclerview/VideoListViewHolder.kt
+++ app/src/main/java/com/bytedance/minitiktok/recyclerview/VideoListViewHolder.kt
@@ -15,39 +15,27 @@ import com.bytedance.minitiktok.VideoActivity
 import com.bytedance.minitiktok.model.Video
 import java.text.FieldPosition
 
-class VideoListViewHolder(view: View) : RecyclerView.ViewHolder(view)
-{
+class VideoListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var mView: View = view
-    private var mCard: CardView
-    private var mNameText: TextView
-    private var mDateText: TextView
-    private var mImage: ImageView
+    private var mCard: CardView = view.findViewById(R.id.card)
+    private var mNameText: TextView = view.findViewById(R.id.nameText)
+    private var mDateText: TextView = view.findViewById(R.id.updateDateText)
+    private var mImage: ImageView = view.findViewById(R.id.img)
 
-    init {
-        mCard = view.findViewById(R.id.card)
-        mNameText = view.findViewById(R.id.nameText)
-        mDateText = view.findViewById(R.id.updateDateText)
-        mImage = view.findViewById(R.id.img)
-    }
-
-    companion object
-    {
-        fun create(context: Context, root: ViewGroup): VideoListViewHolder
-        {
-            val v = LayoutInflater.from(context).inflate(R.layout.video_item_view,root,false)
+    companion object {
+        fun create(context: Context, root: ViewGroup): VideoListViewHolder {
+            val v = LayoutInflater.from(context).inflate(R.layout.video_item_view, root, false)
             return VideoListViewHolder(v)
         }
     }
 
-    fun bind(data: Video?, activity: Activity?,position: Int)
-    {
-        if(data == null || activity == null) return
+    fun bind(data: Video?, activity: Activity?, position: Int) {
+        if (data == null || activity == null) return
 
         Glide.with(activity).load(data.imageUrl).into(mImage)
         mNameText.text = data.userName
         mDateText.text = data.updateDate
-        mCard.setOnClickListener(object: View.OnClickListener
-        {
+        mCard.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
                 VideoActivity.launch(activity, data.videoUrl)
             }
