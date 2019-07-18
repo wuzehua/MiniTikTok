4647cbd19ea78fd659dc1d3a1eac76106e834c9e
shenmishajing <shenmishajing@Gmail.com>
2019-07-18 23:38:32 +0800

    fix getString bug

--- app/src/main/java/com/bytedance/minitiktok/recyclerview/VideoListViewAdapter.kt
+++ app/src/main/java/com/bytedance/minitiktok/recyclerview/VideoListViewAdapter.kt
@@ -14,16 +14,14 @@ import com.bytedance.minitiktok.db.DataBase
 import com.bytedance.minitiktok.model.Like
 
 
-class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>
-{
+class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private var items: List<Video> = ArrayList()
     private var likeItems: List<String> = ArrayList()
 
     private var mActivity: Activity?
 
-    constructor(activity: Activity?):super()
-    {
+    constructor(activity: Activity?) : super() {
         mActivity = activity
     }
 
@@ -38,26 +36,21 @@ class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         var liked = false
-        if(likeItems.contains(items[position].videoId))
-        {
+        if (likeItems.contains(items[position].videoId)) {
             liked = true
         }
-        (holder as VideoListViewHolder).bind(items[position], mActivity, position,liked)
+        (holder as VideoListViewHolder).bind(items[position], mActivity, position, liked)
 
-        holder.mLikedButton.setOnClickListener(object : View.OnClickListener
-        {
+        holder.mLikedButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
 
                 object : AsyncTask<String, Int, String>() {
                     override fun doInBackground(vararg strings: String): String {
                         val sharedPreference = mActivity?.getSharedPreferences("MiniTikTok", Context.MODE_PRIVATE)!!
-                        val userName = sharedPreference.getString("user_name",getString(R.string.un_registe_user_name))
-                        if(liked)
-                        {
+                        val userName = sharedPreference.getString("user_name", "un_registe_user_name")
+                        if (liked) {
                             DataBase.getInstance(mActivity!!).deleteLike(userName, items[position].videoId)
-                        }
-                        else
-                        {
+                        } else {
                             val temp = Like()
                             temp.user_name = userName
                             temp.video_id = items[position].videoId
@@ -69,11 +62,9 @@ class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>
 
                     override fun onPostExecute(s: String) {
                         super.onPostExecute(s)
-                        if (liked)
-                        {
+                        if (liked) {
                             holder.mLikedButton.progress = 0f
-                        }else
-                        {
+                        } else {
                             holder.mLikedButton.speed = 2f
                             holder.mLikedButton.playAnimation()
                         }
@@ -89,13 +80,11 @@ class VideoListViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>
         (holder as VideoListViewHolder).mLikedButton.cancelAnimation()
     }
 
-    fun setItems(value: List<Video>)
-    {
+    fun setItems(value: List<Video>) {
         items = value
     }
 
-    fun setLikeItems(value: List<String>)
-    {
+    fun setLikeItems(value: List<String>) {
         likeItems = value
     }
 }
